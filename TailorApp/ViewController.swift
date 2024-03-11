c80c4acfbe6db6d3e1121f100a918bd43fdfe080
Othman <alkousothman@gmail.com>
2024-03-11 11:44:12 +0300

    Uploading Files

---
+++ TailorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TailorApp
+//
+//  Created by Othman Alkous on 3/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
