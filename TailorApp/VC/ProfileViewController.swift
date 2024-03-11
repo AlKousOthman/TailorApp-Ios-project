54fa4f82710d2968e0d4bbd88d0a9e64404edf9a
razanalshatti <83327@kfh.com>
2024-03-11 14:58:13 +0300

    Razan -> ProfileView

--- TailorApp/VC/ProfileViewController.swift
+++ TailorApp/VC/ProfileViewController.swift
@@ -6,24 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController: UIViewController {
+    
+    var userImage = UIImageView()
+    var usernameLabel = UILabel()
+    var emailLabel = UILabel()
+    var phoneLabel = UILabel()
+    var addressLabel = UILabel()
+    var signoutButton =  UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpUI()
+        setupViews()
+        AutoLAyout()
+        
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpUI(){
+//        signoutButton.setTitle("sign out", for: .normal)
+//        signoutButton.backgroundColor = .lightGray
+//        signoutButton.setTitleColor(.black, for: .normal)
+//        signoutButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+//        signoutButton.layer.cornerRadius = 10
+//        signoutButton.layer.borderWidth = 1
+//        signoutButton.layer.borderColor = UIColor.white.cgColor
+        
+        let powerImage = UIImage(systemName: "power")
+                
+                // Set the image for the button
+                signoutButton.setImage(powerImage, for: .normal)
+                
+                // Customize other properties of the button
+                signoutButton.backgroundColor = .lightGray
+                signoutButton.tintColor = .black // Change the color of the symbol
+                signoutButton.layer.cornerRadius = 10
+                signoutButton.layer.borderWidth = 1
+                signoutButton.layer.borderColor = UIColor.white.cgColor
+        
+        
     }
-    */
+    
+    func setupViews(){
+        view.backgroundColor = .white
+
+        view.addSubview(userImage)
+        view.addSubview(usernameLabel)
+        view.addSubview(emailLabel)
+        view.addSubview(phoneLabel)
+        view.addSubview(addressLabel)
+        view.addSubview(signoutButton)
+        
+    }
+    
+    func AutoLAyout(){
+
+        signoutButton.snp.makeConstraints { make in
+           //make.top.equalTo(view.safeAreaLayoutGuide).offset(-100)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(300)
+            make.width.equalToSuperview().multipliedBy(0.4)
+            make.height.equalTo(50)
+            
+        }
+        
+        
+    }
+    
+
+
 
 }
