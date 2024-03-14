c4fe3672ca7d7790f7aad9870ccf0ca800b0d87e
nouraaldaihani <you@example.com>
2024-03-14 14:32:17 +0300

    UPDATED IMAGES

--- TailorApp/VC/SignInViewController.swift
+++ TailorApp/VC/SignInViewController.swift
@@ -11,87 +11,138 @@ import SnapKit
 class SignInViewController: UIViewController {
     
     var usernameTextField = UITextField()
-    var passwordTextField = UITextField()
-    var forgetPasswordButton = UIButton(type: .system)
-    var registerHereButton = UIButton(type: .system)
-    var signInButton = UIButton(type: .system)
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setUpUI()
-        setupViews()
-        AutoLayout()
-    }
-    
+      var passwordTextField = UITextField()
+      var forgetPasswordButton = UIButton(type: .system)
+      var registerHereButton = UIButton(type: .system)
+      var signInButton = UIButton(type: .system)
+      var logoImageView = UIImageView() // Step 1: Define UIImageView property
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.backgroundColor = .white
+          setUpUI()
+          setupViews()
+          AutoLayout()
+      }
+      
     func setUpUI() {
-        // Set up text fields
-        usernameTextField.placeholder = "Username"
-        passwordTextField.placeholder = "Password"
-        passwordTextField.isSecureTextEntry = true
+            // Set up text fields
+            usernameTextField.placeholder = "Username"
+            passwordTextField.placeholder = "Password"
+            passwordTextField.isSecureTextEntry = true
+            
+            // Set up forget password button
+            forgetPasswordButton.setTitle("Forget password?", for: .normal)
+            forgetPasswordButton.addTarget(self, action: #selector(forgetPasswordButtonTapped), for: .touchUpInside)
+            
+            // Set up register here button
+            registerHereButton.setTitle("Don't have an account? Register here", for: .normal)
+            registerHereButton.addTarget(self, action: #selector(registerHereButtonTapped), for: .touchUpInside)
+            
+            signInButton.setTitle("Sign In", for: .normal)
+            signInButton.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
+            signInButton.frame = CGRect(x: 50, y: 200, width: 200, height: 50)
+            signInButton.backgroundColor = .blue
+            signInButton.setTitleColor(.white, for: .normal)
+            signInButton.layer.cornerRadius = 10
+            signInButton.layer.shadowColor = UIColor.black.cgColor
+            signInButton.layer.shadowOpacity = 0.5
+            signInButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+            signInButton.layer.shadowRadius = 4
+            
+            // Step 2: Set up UIImageView
+            logoImageView.image = UIImage(named: "tailor5")
+        }
         
-        // Set up forget password button
-        forgetPasswordButton.setTitle("Forget password?", for: .normal)
-        forgetPasswordButton.addTarget(self, action: #selector(forgetPasswordButtonTapped), for: .touchUpInside)
+        func setupViews() {
+            // Add subviews
+            view.addSubview(usernameTextField)
+            view.addSubview(passwordTextField)
+            view.addSubview(forgetPasswordButton)
+            view.addSubview(registerHereButton)
+            view.addSubview(signInButton)
+            view.addSubview(logoImageView)
+        }
         
-        // Set up register here button
-        registerHereButton.setTitle("Don't have an account? Register here", for: .normal)
-        registerHereButton.addTarget(self, action: #selector(registerHereButtonTapped), for: .touchUpInside)
-        
-        signInButton.setTitle("Sign In", for: .normal)
-        signInButton.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
-        signInButton.frame = CGRect(x: 50, y: 200, width: 200, height: 50)
-        signInButton.backgroundColor = .blue
-        signInButton.setTitleColor(.white, for: .normal)
-        signInButton.layer.cornerRadius = 10
-        signInButton.layer.shadowColor = UIColor.black.cgColor
-        signInButton.layer.shadowOpacity = 0.5
-        signInButton.layer.shadowOffset = CGSize(width: 0, height: 2)
-        signInButton.layer.shadowRadius = 4
-    }
     
-    func setupViews() {
-        // Add subviews
-        view.addSubview(usernameTextField)
-        view.addSubview(passwordTextField)
-        view.addSubview(forgetPasswordButton)
-        view.addSubview(registerHereButton)
-        view.addSubview(signInButton)
-
-    }
-    
+//    func AutoLayout() {
+//          
+//          usernameTextField.snp.makeConstraints { make in
+//              make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+//              make.leading.equalToSuperview().offset(50)
+//              make.trailing.equalToSuperview().offset(-50)
+//              make.height.equalTo(50)
+//          }
+//          
+//        
+//          passwordTextField.snp.makeConstraints { make in
+//              make.top.equalTo(usernameTextField.snp.bottom).offset(20)
+//              make.leading.trailing.height.equalTo(usernameTextField)
+//          }
+//          
+//     
+//          signInButton.snp.makeConstraints { make in
+//              make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+//              make.leading.trailing.height.equalTo(usernameTextField)
+//          }
+//          
+//          
+//          forgetPasswordButton.snp.makeConstraints { make in
+//              make.top.equalTo(signInButton.snp.bottom).offset(10)
+//              make.leading.equalToSuperview().offset(50)
+//              make.trailing.equalToSuperview().offset(-50)
+//          }
+//          
+//          
+//          registerHereButton.snp.makeConstraints { make in
+//              make.centerX.equalToSuperview()
+//              make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-50)
+//          }
+//          
+//          
+//          logoImageView.snp.makeConstraints { make in
+//              make.centerX.equalToSuperview()
+//              make.bottom.equalTo(usernameTextField.snp.top).offset(-10)
+//              make.width.equalTo(300)
+//              make.height.equalTo(200)
+//          }
+//    
+//      }
     func AutoLayout() {
-        // Username Text Field
         usernameTextField.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
-            make.leading.equalToSuperview().offset(50)
+           // make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+                 make.leading.equalToSuperview().offset(50)
             make.trailing.equalToSuperview().offset(-50)
+            make.top.equalTo(logoImageView.snp.bottom).offset(20)      
             make.height.equalTo(50)
         }
         
-        // Password Text Field
         passwordTextField.snp.makeConstraints { make in
             make.top.equalTo(usernameTextField.snp.bottom).offset(20)
             make.leading.trailing.height.equalTo(usernameTextField)
         }
         
-        // Sign In Button
         signInButton.snp.makeConstraints { make in
             make.top.equalTo(passwordTextField.snp.bottom).offset(20)
             make.leading.trailing.height.equalTo(usernameTextField)
         }
         
-        // Forget Password Button
         forgetPasswordButton.snp.makeConstraints { make in
             make.top.equalTo(signInButton.snp.bottom).offset(10)
             make.leading.equalToSuperview().offset(50)
             make.trailing.equalToSuperview().offset(-50)
         }
         
-        // Register Here Button
         registerHereButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-50)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.top).offset(150)
+            make.width.equalTo(300)
+            make.height.equalTo(200)
         }
     }
 
